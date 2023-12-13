76245fb9c993e936a78ec8ba1697fd7da9919c5f
Arash <38922203+arashsheyda@users.noreply.github.com>
2023-12-13 11:58:29 -0700

    chore: test to resolve user schema error

--- server/api/posts/index.get.ts
+++ server/api/posts/index.get.ts
@@ -1,4 +1,6 @@
 export default defineEventHandler(async (event) => {
+  // test temporarily to register UserSchema for error => Schema hasn't been registered for model "User". Use mongoose.model(name, schema)
+  UserSchema
   try {
     return await PostSchema.find({}).populate({
       path: 'user',
