68ff731e54a96eb993071156da72a775e106a26b
Arash <38922203+arashsheyda@users.noreply.github.com>
2023-12-13 20:29:45 -0700

    fix: bg color

--- pages/[post].vue
+++ pages/[post].vue
@@ -54,7 +54,7 @@ async function update() {
 
 <template>
   <UPage class="relative">
-    <div class="post relative m-4 rounded-lg" :class="editing ? 'border border-cyan-400' : 'p-4 bg-gray-950 min-h-screen'">
+    <div class="post relative m-4 rounded-lg" :class="editing ? 'border border-cyan-400' : 'p-4 bg-gray-100 dark:bg-gray-950 min-h-screen'">
       <template v-if="pending">
         <div class="flex flex-col gap-8">
           <USkeleton
