2786703dc826e2507a5675088894f8b7a176e8a4
Arash Ari Sheyda <38922203+arashsheyda@users.noreply.github.com>
2025-05-01 16:38:50 -0600

    chore: null safety for user.email

--- components/Card.vue
+++ components/Card.vue
@@ -30,7 +30,7 @@ function deleteItem() {
     :description="item?.description"
   >
     <div class="absolute right-4 flex gap-2">
-      <template v-if="user.email === item.user.email">
+      <template v-if="user.email === item.user?.email">
         <UTooltip text="Delete">
           <UButton
             class="opacity-10 group-hover:opacity-100 transition-all duration-500"
@@ -59,7 +59,7 @@ function deleteItem() {
       </template>
       <span v-else class="flex items-center gap-1 text-sm opacity-40">
         <UIcon name="i-ph-user-duotone" />
-        {{ item.user.email }}
+        {{ item.user?.email }}
       </span>
     </div>
 
